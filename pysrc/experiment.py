73d92c2d3695c83c2d230ababc532e6c70736060
Alex Kearney <konga.kearney@gmail.com>
2016-11-08 15:49:32 -0700

    learning code

---
+++ pysrc/experiment.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+__author__ = 'kongaloosh'
+
+
+class BanditExperiment(object):
+
+    def __init__(self, number_of_arms):
+        """Creates an n-armed bandit"""
+        self.bandit_means = np.zeros(number_of_arms)
+
+    def action(self, arm_number):
+        """Returns a value for a specific ban<"""
+        return np.random.normal(loc=self.bandit_means[arm_number], scale=1.0)
+
+    def random_walk(self):
+        """Randomly moves all of the arms"""
+        walk = np.random.normal(loc=0, scale=1.0, size=(len(self.bandit_means)))
+        self.bandit_means += walk
+
+    def optimal_action(self):
+        """Returns the action with the best return. """
+        return np.argmax(self.bandit_means)
---
+++ pysrc/learning.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+__author__ = 'kongaloosh'
+
+
+class BanditSampleAverage(object):
+
+    def __init__(self, number_of_arms, epsilon):
+        self.number_of_steps = 0
+        self.bandit_estimates = np.zeros(number_of_arms)
+        self.bandit_visits = np.zeros(number_of_arms)
+        self.epsilon = epsilon
+
+    def get_action(self):
+        if self.epsilon >= np.random.random():
+            return np.random.randint(len(self.bandit_estimates))
+        return np.argmax(self.bandit_estimates)
+
+    def update_average(self, arm, observation):
+        self.bandit_visits[arm] += 1.
+        self.bandit_estimates[arm] += (1/self.bandit_visits[arm]) * (observation - self.bandit_estimates[arm])
+
+
+class SimpleBandit(object):
+
+    def __init__(self, number_of_arms, epsilon, step_size):
+        self.number_of_steps = 0
+        self.bandit_estimates = np.zeros(number_of_arms)
+        self.epsilon = epsilon
+        self.step_size = step_size
+
+    def get_action(self):
+        if self.epsilon <= np.random.random():
+            return np.random.randint(len(self.bandit_estimates))
+        return np.argmax(self.bandit_estimates)
+
+    def update_average(self, arm, observation):
+        self.number_of_steps += 1
+        self.bandit_estimates[arm] += self.step_size * (observation - self.bandit_estimates[arm])
+
