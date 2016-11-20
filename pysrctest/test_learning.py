ed93e7888114a1c121978ac21084551aea3d4fcf
Alex Kearney <konga.kearney@gmail.com>
2016-11-20 12:57:38 -0700

    added unit tests

---
+++ pysrctest/test_learning.py
@@ -0,0 +1,28 @@
+import unittest
+from pysrc.learning import BanditSampleAverage, SimpleBandit
+__author__ = 'kongaloosh'
+
+
+class Test_Sample_Average(unittest.TestCase):
+
+    def test_init(self):
+        pass
+
+    def test_get_action(self):
+        pass
+
+    def test_update(self):
+        pass
+
+
+class Test_Simple_Bandit(unittest.TestCase):
+
+    def test_init(self):
+        pass
+
+    def test_get_action(self):
+        pass
+
+    def update_average(self):
+        pass
+
