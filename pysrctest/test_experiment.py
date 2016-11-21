d63c197ab163f8b43094c09fa6fed727b0154dd4
Alex Kearney <konga.kearney@gmail.com>
2016-11-20 20:46:33 -0700

    added tests to verify bandit problem

--- pysrctest/test_experiment.py
+++ pysrctest/test_experiment.py
@@ -1,1 +1,16 @@
+import unittest
+from pysrc.experiment import BanditExperiment
+from numpy.testing import assert_allclose
+
 __author__ = 'kongaloosh'
+
+class TestBandit(unittest.TestCase):
+
+    def test_init(self):
+        bandit = BanditExperiment(2)
+        self.assertEqual(len(bandit.bandit_means), 2)
+        assert_allclose(bandit.bandit_means, [0, 0])
+
+
+if __name__ == "__main__":
+    unittest.main()
